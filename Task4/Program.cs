7dc9a48b6b3e57cbdc04e274f8edd4ab4ab0bcba
Evgenia Afanasieva <electrojanet@gmail.com>
2023-02-13 19:53:35 +0300

    Added folder Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,77 @@
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся 
+// двузначных чисел. Напишите программу, которая будет построчно 
+// выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+
+int[,,] array3D = Generate3DArray(2, 2, 2);
+Print3DArray(array3D);
+
+int[,,] Generate3DArray(int m, int n, int p)
+{
+    int[,,] result = new int[m, n, p];
+
+    Random rnd = new Random();
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            for (int k = 0; k < result.GetLength(2); k++)
+            {
+                int number = 0;
+                do
+                {
+                    number = rnd.Next(10, 100);
+                }
+                while(IsContain(result, number));
+
+                result[i, j, k] = number;
+            }
+        }
+    }
+
+    return result;
+}
+
+bool IsContain(int[,,] arr, int number)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                if (arr[i, j, k] == number)
+                {
+                    return true;
+                }
+                else if (arr[i, j, k] == 0)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+
+    return false;
+}
+
+void Print3DArray(int[,,] arr)
+{
+    for (int k = 0; k < arr.GetLength(2); k++)
+    {
+        for (int i = 0; i < arr.GetLength(1); i++)
+        {
+            for (int j = 0; j < arr.GetLength(0); j++)
+            {
+                Console.Write($"{arr[i, j, k]}({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
