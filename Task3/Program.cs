f2307965c6986d0d3ff380484125b52e9adcc029
Evgenia Afanasieva <electrojanet@gmail.com>
2023-02-13 19:52:40 +0300

    Added folder Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, 
+// которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+
+int[,] matrixA = Generate2DArray(2, 3);
+int[,] matrixB = Generate2DArray(3, 3);
+Print2DArray(matrixA);
+System.Console.WriteLine("-----------------------------------------");
+Print2DArray(matrixB);
+System.Console.WriteLine("-----------------------------------------");
+Print2DArray(MultipleMatrix(matrixA, matrixB));
+
+
+int[,] MultipleMatrix(int[,] matrixA, int[,] matrixB)
+{
+    int[,] result = new int[matrixA.GetLength(0), matrixB.GetLength(1)];
+
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrixA.GetLength(1); k++)
+            {
+                result[i, j] += matrixA[i, k] * matrixB[k, j];
+            }
+        }
+    }
+
+    return result;
+}
+
+int[,] Generate2DArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = new Random().Next(1, 7);
+        }
+    }
+
+    return result;
+}
+
+void Print2DArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
